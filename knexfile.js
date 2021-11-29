7827086657565c57d7703f8408f78626ac24a649
Paul Reynolds <usuallypaulreynolds@gmail.com>
2021-11-29 13:39:11 -0600

    Finished with module 35.5 Seeding data with Knex

--- knexfile.js
+++ knexfile.js
@@ -11,6 +11,9 @@ module.exports = {
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
   },
 
   staging: {
