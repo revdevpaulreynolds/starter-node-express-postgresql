ae033ae6119dac160950a14b11ecfff6f01623b0
Paul Reynolds <usuallypaulreynolds@gmail.com>
2021-11-29 11:44:09 -0600

    Migrations fun

---
+++ knexfile.js
@@ -0,0 +1,48 @@
+// Update with your config settings.
+const path = require("path");
+require("dotenv").config();
+const { DATABASE_URL } = process.env;
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
