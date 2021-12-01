e4993f50e443ae85b47f4645bb3b55df1928b59c
Paul Reynolds <usuallypaulreynolds@gmail.com>
2021-12-01 10:40:38 -0600

    Finished 35.9 Joins with Knex

--- src/products/products.service.js
+++ src/products/products.service.js
@@ -1,11 +1,23 @@
 const knex = require("../db/connection");
+const mapProperties = require("../utils/map-properties");
+const addCategory = mapProperties({
+  category_id: "category.category_id",
+  category_name: "category.category_name",
+  category_description: "category.category_description",
+})
 
 function list() {
   return knex("products").select("*");
 }
 
 function read(product_id) {
-  return knex("products").select("*").where({ product_id }).first();
+  return knex("products as p")
+  .join("products_categories as pc", "p.product_id", "pc.product_id")
+  .join("categories as c", "pc.category_id", "c.category_id")
+  .select("p.*", "c.*")
+  .where({ "p.product_id": product_id })
+  .first()
+  .then(addCategory);
 }
 
 function listOutOfStockCount() {
@@ -36,6 +48,7 @@ function listTotalWeightByProduct() {
     )
     .groupBy("product_title", "product_sku");
 }
+
 
 module.exports = {
   list,
